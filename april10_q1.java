ac3aec21912a3b7f785d6d5ceb2c0198486b9191
DAYANAND1325 <95399374+DAYANAND1325@users.noreply.github.com>
2022-04-10 22:00:59 +0530

    Add files via upload
    first commit

---
+++ april10_q1.java
@@ -0,0 +1,11 @@
+public class april10_q1 {
+    public static void main(String[] args) {
+        car bmw=new car(100)
+
+    }
+}
+class car{
+    car(int speed){
+        System.out.println("max speed of car is :"+speed)
+    }
+}
